f436bb1493d2bcba535744b4e27dcc9dc196ab11
Mateusz Żółtak <zozlak@zozlak.org>
2015-05-21 13:29:24 +0200

    Dodanie szkieletu testów dla funkcji zapisz_skalowanie()

---
+++ tests/testthat/test-zapisz_skalowanie.R
@@ -0,0 +1,25 @@
+context('zapisz_skalowanie()')
+
+test_that('zapisz_skalowanie() tworzy skalowanie', {
+  
+})
+
+test_that('zapisz_skalowanie() dostosowuje skalę', {
+  
+})
+
+test_that('zapisz_skalowanie() zapisuje parametry', {
+  
+})
+
+test_that('zapisz_skalowanie() linkuje wskaźniki', {
+  
+})
+
+test_that('zapisz_skalowanie() tworzy pesudotest', {
+  
+})
+
+test_that('zapisz_skalowanie() zapisuje oszacowania uczniów', {
+  
+})
