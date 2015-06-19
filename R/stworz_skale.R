f8a43861801fcac6bc14bc1f105d21da3acfae52
tzoltak <t.zoltak@ibe.edu.pl>
2015-06-19 22:02:41 +0200

    Nowa funkcja stworz_skale_ewd() oraz porządek z importem RODBC i RODBCext

--- R/stworz_skale.R
+++ R/stworz_skale.R
@@ -1,6 +1,9 @@
 #' @title Tworzy w bazie nowa skale i zwraca jej id_skali
 #' @description
-#' _
+#' Funkcja stworz_skale() służy do „zarejestrowania” w bazie nowej skali.
+#' Po dokonaniu takiego „zarejestrowania” należy wprowadzić do bazy informacje
+#' o kryteriach oceny i pseudokryteriach oceny, z których składa się dana skala
+#' - służy do tego funkcja \code{\link{edytuj_skale}}.
 #' @param opis opis skali (zwyczajowo w formacie "ZESPOL;EGZ;LATA", np. "paou;s;2002-2011" lub "ewd;gh_h;2012")
 #' @param rodzaj rodzaj skali (ewd/zrównywanie/ktt)
 #' @param doPrezentacji czy skala ma być oznaczona jako przeznaczona do prezentacji
@@ -8,16 +11,17 @@
 #' @param zrodloDanychODBC nazwa zrodla danych ODBC, ktorego nalezy uzyc
 #' @return [numeric] id_skali utworzonej skali
 #' @export
+#' @importFrom RODBC odbcConnect odbcClose odbcSetAutoCommit odbcEndTran
 #' @import RODBCext
 stworz_skale = function(
-	opis, 
+	opis,
 	rodzaj,
 	doPrezentacji,
-	idTestow, 
+	idTestow,
 	zrodloDanychODBC = 'EWD'
 ){
   stopifnot(
-    is.vector(opis), is.character(opis), length(opis) == 1, opis != '', 
+    is.vector(opis), is.character(opis), length(opis) == 1, opis != '',
     is.vector(rodzaj), is.character(rodzaj), length(rodzaj) == 1, !is.na(rodzaj),
     is.vector(doPrezentacji), is.logical(doPrezentacji), length(doPrezentacji) == 1, !is.na(doPrezentacji),
     is.vector(idTestow), is.numeric(idTestow),
@@ -27,7 +31,7 @@ stworz_skale = function(
 	on.exit({
 	  odbcClose(P)
 	})
-  
+
   idTestow = na.exclude(idTestow)
   stopifnot(
     ! opis %in% .sqlQuery(P, "SELECT DISTINCT opis FROM skale")[, 1],
@@ -35,16 +39,16 @@ stworz_skale = function(
     length(idTestow) > 0,
     all(idTestow %in% .sqlQuery(P, "SELECT id_testu FROM testy")[, 1])
   )
-  
+
 	odbcSetAutoCommit(P, FALSE)
 	.sqlQuery(P, "BEGIN")
-		
+
   idSkali = .sqlQuery(P, "SELECT nextval('skale_id_skali_seq')")[1, 1]
   zap = "INSERT INTO skale (id_skali, opis, rodzaj_skali, do_prezentacji) VALUES (?, ?, ?, ?)"
   .sqlQuery(P, zap, list(idSkali, opis, rodzaj, doPrezentacji))
   zap = "INSERT INTO skale_testy (id_skali, id_testu) VALUES (?, ?)"
   .sqlQuery(P, zap, list(rep(idSkali, length(idTestow)), idTestow))
-  
+
   odbcEndTran(P, TRUE)
-	return(idSkali)	
+	return(idSkali)
 }
